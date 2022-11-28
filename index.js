bf79fedb028de3abf54f03c2db7036fc106e65d7
hectorocanaperez <102577133+hectorocanaperez@users.noreply.github.com>
2022-11-28 13:49:33 +0100

    ejercicio2-primer commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const app = express()
-const port = 3000
+const port = 3003
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './web/index.html'));
+
 })
 
 app.get('/api/protectedData', (req, res) => {
@@ -15,3 +18,4 @@ app.get('/api/protectedData', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
